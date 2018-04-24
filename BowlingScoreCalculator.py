bd26851e97229ab4e90b6a51fc4ebade892f406d
Martin <mbagge@outlook.com>
2018-04-24 15:01:35 +0200

    - Implemented CalculateFrameSums function - Changed functions pr. new information from tests about status_code 429 (too many requests), so they wait 10 seconds if this occurs.

--- BowlingScoreCalculator.py
+++ BowlingScoreCalculator.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 class BowlingScoreCalculator():
     APIUrl = "";
@@ -8,18 +9,49 @@ class BowlingScoreCalculator():
         self.APIUrl = APIUrl;
 
     def CalculateFrameSums(self, pointsArray):
-        returnArray = [];
-        return "Unimplemented"
+        returnList = [];
+        currentSum = 0;
+        for i,frame in enumerate(pointsArray):
+            if i <= 9:
+                if frame[0]==10 and frame[1]==0:
+                    # print "Strike";
+                    currentSum += (10);
+                    if len(pointsArray)-1 > i:
+                        currentSum += pointsArray[i+1][0]+pointsArray[i+1][1]
+                        if pointsArray[i+1][0]==10 and len(pointsArray)-1 > i+1:
+                            # Two strikes in a row
+                            currentSum += pointsArray[i+2][0];
+
+                elif frame[0]+frame[1]==10:
+                    # print "Spare";
+                    currentSum += 10;
+                    if len(pointsArray)-1 > i:
+                        currentSum += pointsArray[i+1][0];
+
+                else:
+                    # print "Open Frame";
+                    currentSum += (frame[0]+frame[1]);
+
+                returnList.append(currentSum);
+
+        return returnList;
 
     def GetPointsFromAPI(self):
         #Get bowling point frames and API POST Token
         response = requests.get(self.APIUrl);
-
+        if response.status_code == 429:
+            # Too many attempts. Wait a little
+            time.sleep(10);
+            return self.GetPointsFromAPI();
         return response;
 
     def PostSumsToAPI(self,sums,token):
         postmsg = {"points":sums};
         response = requests.post(self.APIUrl,json=postmsg,params={"token":token});
+        if response.status_code == 429:
+            # Too many attempts. Wait a little
+            time.sleep(10);
+            return self.PostSumsToAPI(sums,token);
         return response;
 
 #Class end
@@ -30,6 +62,13 @@ if __name__ == "__main__":
 
     calc = BowlingScoreCalculator();
     JSON = calc.GetPointsFromAPI().json();
-    response = calc.PostSumsToAPI(JSON["points"],JSON["token"]);
+    print "Scores gotten from API: ";
+    print JSON["points"];
+    print "";
+    sums = calc.CalculateFrameSums(JSON["points"]);
+    print "Calculated sums: "
+    print sums;
+    print "";
+    response = calc.PostSumsToAPI(sums,JSON["token"]);
     print "Token status code: " + str(response.status_code);
     print "Calculation success: " + str(response.json()["success"]);
